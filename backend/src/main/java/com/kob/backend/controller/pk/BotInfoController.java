30e9e35052b2b37ebc584143254c7f33a3b180dc
jianguo li <a15991242356@163.com>
2025-03-06 18:03:58 +0800

    搭建好的项目骨架

--- backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -1,5 +1,6 @@
 package com.kob.backend.controller.pk;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,24 +8,20 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-
+@CrossOrigin(maxAge = 3600)
 @RestController
 @RequestMapping("/pk/")
 public class BotInfoController {
     @RequestMapping("getbotinfo")
 
-    public List<Map<String,String>> getBotInfo() {
-        List<Map<String,String>> list = new LinkedList<>();
+    public Map<String,String> getBotInfo() {
+
 
         Map<String,String> bot1 = new HashMap<>();
         bot1.put("name","tiger");
         bot1.put("rating","1500");
-        Map<String,String> bot2 = new HashMap<>();
-        bot2.put("name","apple");
-        bot2.put("rating","1800");
-        list.add(bot1);
-        list.add(bot2);
-        return list;
+
+        return bot1;
 
 
 
