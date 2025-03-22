dcb4eecc6ed106d9a449127b21c5ea623eda4113
jianguo li <a15991242356@163.com>
2025-03-23 01:45:43 +0800

    实习导航栏和对战地图

---
+++ web/src/assets/scripts/AcGameObject.js
@@ -0,0 +1,45 @@
+const Ac_GAME_OBJECTS = [];
+export class AcGameObject {
+    constructor(){
+        Ac_GAME_OBJECTS.push(this);
+        this.timedelta = 0;
+        this.has_called_start =false;
+    }
+    start(){
+        //只执行一次
+    }
+    update(){ //每一帧执行一次，出第一次除外
+
+    }
+    on_destroy(){
+        //删除之前操作
+    }
+    destrroy(){
+        this.on_destroy();
+        for(let i in Ac_GAME_OBJECTS){
+            const obj = Ac_GAME_OBJECTS[i];
+            if(obj === this ){
+                Ac_GAME_OBJECTS.splice(i);
+                break;
+            }
+        }
+    }
+}
+
+let last_timestamp ;//上一次执行的时刻
+const step = timestamp => {
+    for (let obj of Ac_GAME_OBJECTS){
+        if( !obj.has_called_start){
+            obj.has_called_start = true
+            obj.start();
+        }
+        else{
+            obj.timedelta = timestamp - last_timestamp;
+            obj.update();
+        }
+    }
+    last_timestamp = timestamp;
+    requestAnimationFrame(step)
+}
+
+requestAnimationFrame(step)
--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,8 +1,44 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
-
+import PkIndexview from '../views/pk/PkIndexview.vue'
+import RanklistIndexview from '../views/ranklist/RanklistIndexview'
+import RecordIndexview from '../views/record/RecordIndexview'
+import UserbotIndexview from '../views/user/bot/UserbotIndexview'
+import NotFound from '../views/error/NotFound'
 const routes = [
-  
+  {path : "/",
+    name : "home",
+    redirect : "/pk/",
+
+  },
+  {path : "/pk/",
+    name : "pk_index",
+    component : PkIndexview,
+
+  },
+  {path : "/404/",
+    name : "notfound_index",
+    component : NotFound,
+
+  },
+  {path : "/ranklist/",
+    name : "ranklist_index",
+    component : RanklistIndexview,
+
+  },
+  {path : "/user/bot/",
+    name : "userbot_index",
+    component : UserbotIndexview,
+
+  },
+  {path : "/record/",
+    name : "record_index",
+    component : RecordIndexview,
+
+  },
+  {path : "/:catchAll(.*)",
+    redirect : "/404/"
+
+  },
   
 ]
 
